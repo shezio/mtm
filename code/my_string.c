1c3c91f94cdd11af49b2c613f9da6e61f070cc4a
sbunzel <shlomo.bunzel@cyberark.com>
2023-03-08 21:30:58 +0200

    closer to end q1

--- code/my_string.c
+++ code/my_string.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <io.h>
-#include <string.h>
+#include "my_string.h"
 
-#define MAX_SIZE 81
-
-int my_strcmp(const char *str1, const char *str2);
-int my_strncmp(const char *str1, const char *str2, int n);
-int my_strchr(const char *str, int c);
+int my_strcmp(char[], char[]);
+int my_strncmp(char[], char[],int);
+int my_strchr(char[], char);
 
 int main(int argc, char *argv[]) {
 	char str1[MAX_SIZE];
 	char str2[MAX_SIZE];
-    int n;
-    char c;
+    int num_of_chars_to_compare;
+    int selection = 0;
+    char char_to_search_for_index;
 
-    // Check if a file is specified or input is from stdin
+    /* Check if a file is specified or input is from stdin */
     FILE *fp;
     if (argc == 2) {
         fp = fopen(argv[1], "r");
@@ -28,22 +26,55 @@ int main(int argc, char *argv[]) {
     }
 
 	if (fp == stdin) {
-        // no input redirection, use scanf
-	    printf("\n Please enter input appropriate to get the result u seek:");
-        printf("\n To compare length of 2 strings pls insert 2 strings with max size of:(max %d characters): ", MAX_SIZE-1);
-        printf("\n To compare the first n chars of 2 strings pls insert 2 strings with max size of (max %d characters) and an a number smaller or equal to (max %d characters):", MAX_SIZE-1, MAX_SIZE-1);
-        printf("\n To find the index of a character that exists in a string pls provide a string with max size of (max %d characters)  and a char u wish to find:",MAX_SIZE-1);
+        printf("\nPlease select a function:");
+        printf("\n To compare length of 2 strings using my_strcmp function please select (1) ");
+        printf("\n To compare the first N characters of 2 strings using my_strncmp function please select (2) ");
+        printf("\n To find the location of a character in a string using my_strchr function please select (3) ");
+        printf("\nInput your selection please: ");
+        scanf("%d",&selection);
+        switch(selection)
+        {
+            case 1: /*my_strcmp function:*/
+                printf("\n Please insert 2 strings, each with max size of:(max %d characters): ", MAX_SIZE-1);
+                printf("\nInput the first string: ");
+                scanf("%s",str1);
+                printf("\nInput the second string: ");
+                scanf("%s",str2);
+                printf("\nThe result is: %d\n",my_strcmp(str1, str2));
+                break;		
+            case 2: /*my_strncmp function:*/
+                printf("\nPlease insert 2 strings, each with max size of (max %d characters) and a number not greater than (max %d characters):", MAX_SIZE-1, MAX_SIZE-1);
+                printf("\nInput the first string: ");
+                scanf("%s",str1);
+                printf("\nInput the second string: ");
+                scanf("%s",str2);
+                printf("\nInput the number of characters to compare: ");
+                scanf("%d",&num_of_chars_to_compare);
+                printf("\nThe result is: %d\n",my_strncmp(str1, str2, num_of_chars_to_compare));
+                break;
+            case 3:	/*my_strchr function:*/
+                printf("\nmy_strchr function:");
+                printf("\nType string to the str: ");
+                scanf("%s",str1);
+                printf("Type char to get index of: ");
+                scanf("%c", &char_to_search_for_index);
+                printf("\nThe result is: %d\n",my_strchr(str1, char_to_search_for_index));
+                break;
+            default:/*non of the above function:*/
+                printf("\nYour selection didn\'t feet to any of our function.\n");
+                break;
+	}
+
         if (scanf("%s", str1) != 1) {
             printf("Error reading input\n");
             return 1;
         }
-	    printf("\nEnter string 2 (max %d characters): ", MAX_SIZE-1);
         if (scanf("%s", str2) != 1) {
             printf("Error reading input\n");
             return 1;
         }
     } else {
-        // input redirection, use fgets
+        /*input redirection, use fgets*/ 
         if (fgets(str1, MAX_SIZE, stdin) == NULL) {
             printf("Error reading input\n");
             return 1;
@@ -55,65 +86,12 @@ int main(int argc, char *argv[]) {
     
     }
 
-    // Determine which function to use based on input
-    int num_inputs = fscanf(fp, "%s %s %d %c", str1, str2, &n, &c);
-    if (num_inputs == 2) {
-        printf("Using my_strcmp.\n");
-        int result = my_strcmp(str1, str2);
-        printf("Result: %d\n", result);
-    } else if (num_inputs == 3) {
-        printf("Using my_strncmp.\n");
-        int result = my_strncmp(str1, str2, n);
-        printf("Result: %d\n", result);
-    } else if (num_inputs == 4) {
-        printf("Using my_strchr.\n");
-        int result = my_strchr(str1, c);
-        if (result == -1) {
-            printf("Character not found.\n");
-        } else {
-            printf("Result: %d\n", result);
-        }
-    } else {
-        printf("Invalid input.\n");
-    }
-
-    // Close file if necessary
+    /* Close file if necessary*/
     if (argc == 2) {
         fclose(fp);
     }
 
-	return 0;
-}
-int my_strcmp(const char *str1, const char *str2) {
-    while (*str1 == *str2) {
-        if (*str1 == '\0')
-            return 0;
-        str1++;
-        str2++;
-    }
-    return (*(unsigned char*)str1 < *(unsigned char*)str2) ? -1 : 1;
-}
-int my_strncmp(const char *str1, const char *str2, int n) {
-    if (n == 0)
-        return 0;
-    while (n-- != 0) {
-        if (*str1 != *str2)
-            return (*(unsigned char*)str1 - *(unsigned char*)str2);
-        if (n == 0 || *str1 == '\0' || *str2 == '\0')
-            break;
-        str1++;
-        str2++;
-    }
     return 0;
+	
 }
-int my_strchr(const char *str, int c) {
-    int index = 0;
-    while (*str != '\0') {
-        if (*str == (char)c) {
-            return index;
-        }
-        str++;
-        index++;
-    }
-    return -1;
-}
+
